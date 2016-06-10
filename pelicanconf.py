04cdeabfb1a5ceb015e3eebae156eeefd1636f8a
Adam Beckmeyer <abeckmeyer@auburn.edu>
2016-06-09 21:31:35 -0400

    using new theme pujangga

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Kristin'
 SITENAME = "Kristin's Stuff"
-SITEURL = ''
+SITESUBTITLE = 'All the stuff that Kristin likes'
+SITEURL = 'https://my-true-lover.github.io'
 
-THEME = '/home/us11334/.pelican/themes/blue-penguin'
+THEME = './theme'
 PLUGIN_PATHS = ['/home/us11334/.pelican/plugins']
 PLUGINS = ['photos']
 
@@ -22,25 +23,36 @@ TIMEZONE = 'EST'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/index.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+FEED_ALL_RSS = 'rss/index.xml'
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Pujangga', 'https://github.com/habibillah/pujangga'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/RistinEigh'),
+          ('Github', 'https://github.com/my-true-lover'),)
 
 DEFAULT_PAGINATION = 10
+DISPLAY_PAGES_ON_MENU = True
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
 STATIC_PATHS = ['images']
+
+ARTICLE_URL = 'posts/{slug}/'
+
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
+DAY_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/index.html'
+
+TWITTER_USERNAME = 'RistinEigh'
